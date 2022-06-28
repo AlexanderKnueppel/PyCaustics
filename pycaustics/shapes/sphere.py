9f0ab9a108d8b855aa4be9676edff13dd0428996
Alexander Knueppel <a.knueppel@tu-bs.de>
2022-06-28 15:49:36 +0200

    Return point of collision

--- pycaustics/shapes/sphere.py
+++ pycaustics/shapes/sphere.py
@@ -6,13 +6,16 @@ class Sphere:
         self.center = center
         self.radius = radius
 
-    def hit(self, ray:Ray) -> bool:
+    def hit(self, ray:Ray) -> float:
         OC = ray.origin - self.center
         a = dot(ray.direction,ray.direction)
         b = 2.0*dot(ray.direction, OC)
         c = dot(OC,OC) - self.radius**2
         d = b*b - 4*a*c # based on PQ-Formula; has discremenant a real solution? 
-        return d>=0
+        if d < 0: 
+            return -1
+
+        return (-b-math.sqrt(d)) / (2*a)
 
     def color(self) -> Color:
         return Color(1,0,0)
