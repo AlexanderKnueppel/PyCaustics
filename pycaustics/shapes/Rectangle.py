5d11760df491f101b9522353c4258c81ad3b03e7
Alexander Knueppel <a.knueppel@tu-bs.de>
2022-07-02 21:52:21 +0200

    Start with arbitrary rectangles

--- pycaustics/shapes/Rectangle.py
+++ pycaustics/shapes/Rectangle.py
@@ -3,11 +3,14 @@ from core.vec3 import *
 from numpy import array
 
 class RectangleAA:
-    """ Start with Axis-Aligned Rectangle """
+    """ Start with Axis-Aligned Rectangle (prob. super-slow) """
     def __init__(self, lowerleft = Point3(-0.5,-0.5,-1), upperright = Point3(0.5,0.5,-1)) -> None:
         self.lowerleft = lowerleft
         self.upperright = upperright
         self.normal = Vec3(0,0,1)
+        self.scale = Vec3(1.0,1.0,1.0)
+        self.translate = Vec3(0.0,0.0,0.0)
+        self.rotate = Vec3(0.0,0.0,0.0)
 
     def hit(self, ray:Ray) -> float:
         # test1 = dot(normalize(ray.direction), self.normal)
@@ -27,17 +30,40 @@ class RectangleAA:
         #         print(f"{hit_point}")
         #         return t
 
-        rectZ = self.lowerleft.z
-        dir = normalize(ray.direction)
+        #apply scale/translate
+        minPoint = Vec3(self.scale.x*self.lowerleft + self.translate.x, 
+                        self.scale.y*self.lowerleft + self.translate.y, 
+                        self.scale.y*self.lowerleft + self.translate.y)
+
+        maxPoint = Vec3(self.scale.x*self.upperright + self.translate.x, 
+                        self.scale.y*self.upperright + self.translate.y, 
+                        self.scale.y*self.upperright + self.translate.y)
+
+        rectZ = minPoint.z
+        rayDir = normalize(ray.direction)
+        rayOrigin = ray.origin
+        
+        # Apply rotation
+
         t = (rectZ - ray.origin.z) / dir.z
         hit_point = ray.origin + t*dir
 
-        hit = hit_point.x <= self.upperright.x and hit_point.x >= self.lowerleft.x and hit_point.y <= self.upperright.y and hit_point.y >= self.lowerleft.y
+        hit = hit_point.x <= maxPoint.x and hit_point.x >= minPoint.x and hit_point.y <= maxPoint.y and hit_point.y >= minPoint.y
 
         if hit:
             return t
         else:
             return -1
+
+    def scale(self, x = 1.0, y = 1.0, z = 1.0):
+        self.scale = Vec3(x,y,z)
+
+    def translate(self, x = 0.0, y = 0.0, z = 0.0):
+        self.translate = Vec3(x,y,z)
+
+    def rotate(self, x = 0.0, y = 0.0, z = 0.0):
+        self.rotate = Vec3(x,y,z)
+
 
     def color(self, hit_point) -> Color:
         N = normalize(hit_point - self.lowerleft)
